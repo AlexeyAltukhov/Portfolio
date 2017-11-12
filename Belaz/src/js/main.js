21afb9c050ecdf3c6bb46f827033b337c40020f7
Alexey Altukhov <al.altukhov@gmail.com>
2017-11-13 00:03:37 +0300

    добавил блок "Вам не придется ждать"

---
+++ Belaz/src/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.features-list').slick({
+    infinite: true,
+    //dots: true,
+    //arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
