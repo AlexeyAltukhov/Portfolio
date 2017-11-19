688718f06db6bd0d7f0943f585dea19f0bd69da1
Alexey Altukhov <al.altukhov@gmail.com>
2017-11-19 18:19:46 +0300

    верстка оставшихся блоков
    Блоки:
    - С нами удобно работать
    - У нас собственный склад
    - Сервисное обслуживание
    - Необходима консультация
    - Приезжайте к нам в офис
    - Google карта
    - Подвал сайта

--- Belaz/src/js/main.js
+++ Belaz/src/js/main.js
@@ -20,4 +20,55 @@ $(document).ready(function(){
       }
     ]
   });
+  $('.comfort-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.storage-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="prev arrow"></button>',
+    nextArrow: '<button class="next arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.services-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
